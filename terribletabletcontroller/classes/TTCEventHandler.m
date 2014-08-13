b825dbac293ef5ab9f8a4738b02cda696c42dc0e
Ben Roth <spongebucket@gmail.com>
2014-08-13 16:58:04 -0700

    minimal parsing of incoming mouse/tablet events

--- terribletabletcontroller/classes/TTCEventHandler.m
+++ terribletabletcontroller/classes/TTCEventHandler.m
@@ -7,6 +7,12 @@
 
 #import "TTCEventHandler.h"
 
+@interface TTCEventHandler ()
+
+- (void) printEventType: (NSEvent *)event;
+
+@end
+
 @implementation TTCEventHandler
 
 + (NSEventMask) acceptableEventMask
@@ -19,9 +25,67 @@
 
 - (NSEvent *) handleEvent:(NSEvent *)event
 {
-    // TODO
+    [self printEventType:event];
     
     return event;
+}
+
+
+#pragma mark internal methods
+
+- (void) printEventType:(NSEvent *)event
+{
+    NSString *printableType = @"", *printableSubtype = @"";
+    
+    switch (event.type) {
+        case NSMouseMoved:
+            printableType = @"moved";
+            break;
+        case NSLeftMouseDown:
+            printableType = @"left down";
+            break;
+        case NSLeftMouseUp:
+            printableType = @"left up";
+            break;
+        case NSLeftMouseDragged:
+            printableType = @"left dragged";
+            break;
+        case NSOtherMouseDown:
+            printableType = @"other down";
+            break;
+        case NSOtherMouseUp:
+            printableType = @"other up";
+            break;
+        case NSOtherMouseDragged:
+            printableType = @"other dragged";
+            break;
+        case NSTabletPoint:
+            printableType = @"point";
+            break;
+        case NSTabletProximity:
+            printableType = @"prox";
+            break;
+        default:
+            printableType = [NSString stringWithFormat:@"OTHER: %lu", (unsigned long)event.type];
+            break;
+    }
+    
+    switch (event.subtype) {
+        case NSMouseEventSubtype: case NSTouchEventSubtype:
+            // on a macbook, trackpad events mysteriously count as "touch"
+            printableSubtype = @"default/touch";
+            break;
+        case NSTabletPointEventSubtype:
+            printableSubtype = @"point";
+            break;
+        case NSTabletProximityEventSubtype:
+            printableSubtype = @"prox";
+            break;
+        default:
+            break;
+    }
+    
+    NSLog(@"%@ : %@", printableType, printableSubtype);
 }
 
 @end
